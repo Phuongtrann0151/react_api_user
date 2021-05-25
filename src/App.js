1af58d4d91d63ad8aebfe3d165a033d6a55f5614
Phtran0151 <phuong.tran@tomosia.com>
2021-05-25 09:23:59 +0700

    component,props and state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Hearder from '../src/components/Layout/Header.js'
 import Footer from '../src/components/Layout/Footer.js'
+import TodoList from '../src/components/TodoList/Todo.jsx'
 
 export default function App() {
   return (
     <div className="App">
       <Hearder/>
+      <TodoList/>
       <Footer/>
     </div>
   )
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,24 +2,49 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Header.css';
 
-export default function Footer(){
-  return (
-    <header>
-      <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand">My App</a>
-        <form class="form-inline">
-          <ul class="nav justify-content-center">
-            <li class="nav-item">
-              <a class="nav-link active" href="">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">Register</a>
-            </li>
-          </ul>
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
-    </header>
-  )
+export default class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      words: [],
+      text: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleClick(e){
+    e.preventDefault();
+    const allWords = {
+      text: this.state.text,
+      id: Date.now()
+    }
+    this.setState(state => ({
+      words: state.items.concat(allWords),
+      text: ''
+    }))
+  }
+  handleChange(e){
+    this.setState({ text: e.target.value })
+  }
+  render(){
+    return (
+      <header>
+        <nav class="navbar navbar-light bg-light justify-content-between" words={this.state.items}>
+          <a class="navbar-brand">My App</a>
+          <form class="form-inline" onSubmit={this.handleClick}>
+            <ul class="nav justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link active" href="">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Register</a>
+              </li>
+            </ul>
+            <input class="form-control mr-sm-2" type="search" placeholder="Type in some words!" value={this.state.text} onChange={this.handleChange} />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit</button>
+          </form>
+        </nav>
+      </header>
+    )
+  }
 }
